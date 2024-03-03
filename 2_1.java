ff8b07411b7a92603222ba6caa436d3c27f4454f
lhwlord <40737955+lhwlord@users.noreply.github.com>
2024-03-03 06:03:01 +0000

    commit

--- 2_1.java
+++ 2_1.java
@@ -1,8 +1,10 @@
-public static void main(String[] args) {
+class test {
+  public static void main(String[] args) {
   java.util.Scanner input = new java.util.Scanner(System.in);
 
   System.out.print("Enter a miles: ");
-  double miles = ______;
-  double kilometers = ______;
-  System.out.println();
+  double miles = input.nextInt();
+  double kilometers = miles * 1.6;
+  System.out.println(miles + " miles is " + kilometers + " kilometers");
+}
 }
